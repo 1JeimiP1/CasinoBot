a00293d81a7e74c0a399c2f352265252ed8335ca
Bogdan <jeimiplivechannel@gmail.com>
2020-12-03 00:01:39 +0200

    added writing users to BD

--- bot.py
+++ bot.py
@@ -16,12 +16,24 @@ bot = Bot(token = os.getenv('TOKEN'))
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-#MongoDB connection
-# client = pymongo.MongoClient(os.getenv('CLIENT'))
+#MongoDB connections
+client = pymongo.MongoClient(os.getenv('CLIENT'))
+database = client['Casino']
+users_mongo = database["users"]
 
 @dp.message_handler(commands=["start"])
 async def start_func(message):
-    await bot.send_message(message.chat.id, "Привет, друже {}".format(message.from_user.first_name))
+    id_check = message.from_user.id
+    res = users_mongo.find_one({"user_id": id_check}) #checking for presence of the user in database
+    if res:
+        pass #user in database
+    else:
+        users_mongo.insert_one({
+               "user_id": message.from_user.id,
+               "first_name": message.from_user.first_name,
+               "username": message.from_user.username
+              }) #writing user to database
+    await bot.send_message(message.chat.id, "<i>Эй, игрок, приходи в казино поиграть.\nТы своим не поверишь глазам!\nЖдёт тебя впереди диффитчентов каскад!\nТы готов?) Проходите в вип-зал!</i>", parse_mode="HTML", reply_to_message_id=message.message_id)
 
 
 if __name__ == '__main__':
